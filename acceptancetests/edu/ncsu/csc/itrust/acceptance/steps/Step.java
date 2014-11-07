61b1bbfe1ed58ac786f07789710e8188d266c5ed
muymoo <matt.momont@gmail.com>
2014-11-06 21:11:54 -0800

    - Added global Before/After with driver - Completed the View All Providers feature - Added picocontainer as DI library for default constructor injection.

--- acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/Step.java
+++ acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/Step.java
@@ -13,15 +13,28 @@ package edu.ncsu.csc.itrust.acceptance.steps;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
 /**
  * Base class that all steps should extend
  */
-public abstract class Step
+public class Step
 {
-    protected WebDriver driver;
+    private static final String ITRUST_URL = "http://localhost:8080/iTrust/";
+    protected WebDriver         browser;
 
-    public Step()
+    @Given("iTrust is running")
+    public void beforeScenario()
     {
-        this.driver = new FirefoxDriver();
+        this.browser = new FirefoxDriver();
+        browser.get(ITRUST_URL);
     }
+
+    @Then("I close the browser")
+    public void afterScenario()
+    {
+        browser.close();
+    }
+
 }
