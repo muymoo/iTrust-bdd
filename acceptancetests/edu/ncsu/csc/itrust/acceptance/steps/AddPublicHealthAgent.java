cef9355434313663d23ba7d442890cb23fb54f5a
lrbridge <lrbridge@users.noreply.github.com>
2014-11-07 21:29:31 -0800

    laurens first super hardcoded test

--- acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/AddPublicHealthAgent.java
+++ acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/AddPublicHealthAgent.java
@@ -12,12 +12,13 @@ package edu.ncsu.csc.itrust.acceptance.steps;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -45,29 +46,29 @@ public class AddPublicHealthAgent
     public void admin_clicks_the_Add_PHA_link_under_the_Add_menu_subheading()
             throws Throwable
     {
-        WebElement addMenu = browser.findElement(By.cssSelector(
-        		".iTrustMenuContents div[anim-type=\"#addMenu\"]"));
-        addMenu.click();
-        
+    	List<WebElement> menus = browser.findElements(By.cssSelector(
+    			".iTrustMenuContents div.panel h2"));
+    	menus.get(0).click();
+    	
         WebElement addPha = browser.findElement(By.linkText("Add PHA"));
         addPha.click();
     }
 
-    @When("^Admin enters: first name: Tim last name: Agent email: pha@timagent\\.com$")
+    @When("^Admin enters: first name: Frank last name: Sinatra email: frankie@gmail\\.com$")
     public void admin_enters_first_name_Tim_last_name_Agent_email_pha_timagent_com()
             throws Throwable
     {
         WebElement firstNameInput = browser.findElement(By.cssSelector(
         		"#iTrustContent input[name=\"firstName\"]"));
-        firstNameInput.sendKeys("Tim");
+        firstNameInput.sendKeys("Frank");
         
         WebElement lastNameInput = browser.findElement(By.cssSelector(
         		"#iTrustContent input[name=\"lastName\"]"));
-        lastNameInput.sendKeys("Agent");
+        lastNameInput.sendKeys("Sinatra");
         
         WebElement emailInput = browser.findElement(By.cssSelector(
         		"#iTrustContent input[name=\"email\"]"));
-        emailInput.sendKeys("pha@timagent.com");
+        emailInput.sendKeys("frankie@gmail.com");
         
         WebElement submitButton = browser.findElement(By.cssSelector(
         		"#iTrustContent input[value=\"Add personnel\"]"));
@@ -83,42 +84,42 @@ public class AddPublicHealthAgent
     	continueToPhaLink.click();
     }
 
-    @When("^Admin enters: Address: (\\d+) Oak Hills Drive City: Capitol City State: North Carolina Zip: (\\d+)-(\\d+) Phone: (\\d+)-(\\d+)-(\\d+)$")
-    public void admin_enters_Address_Oak_Hills_Drive_City_Capitol_City_State_North_Carolina_Zip_Phone(int arg1,
-            int arg2, int arg3, int arg4, int arg5, int arg6)
+    @When("^Admin enters: Address: 123 Oak Hills Drive City: Capitol City State: North Carolina Zip: 28700-0458 Phone: 555-877-5105$")
+    public void admin_enters_Address_Oak_Hills_Drive_City_Capitol_City_State_North_Carolina_Zip_Phone()
             throws Throwable
     {
         WebElement address1 = browser.findElement(By.cssSelector(
-        		"input [name=\"streetAddress1\"]"));
-        address1.sendKeys(arg1 + " Oak Hills Drive");
+        		"input[name=\"streetAddress1\"]"));
+        address1.sendKeys("123 Oak Hills Drive");
         
         WebElement city = browser.findElement(By.cssSelector(
-        		"input [name=\"city\"]"));
+        		"input[name=\"city\"]"));
         city.sendKeys("Capitol City");
         
-        Select state = (Select) browser.findElement(By.cssSelector(
-        		"input [name=\"state\"]"));
+        Select state = new Select(browser.findElement(By.cssSelector(
+        		"select[name=\"state\"]")));
         state.selectByVisibleText("North Carolina");
         
         WebElement zip = browser.findElement(By.cssSelector(
-        		"input [name=\"zip\"]"));
-        zip.sendKeys(arg2 + "-" + arg3); 
+        		"input[name=\"zip\"]"));
+        zip.sendKeys("28700-0458"); 
 
         WebElement phone = browser.findElement(By.cssSelector(
-        		"input [name=\"phone\"]"));
-        phone.sendKeys(arg4 + "-" + arg5 + "-" + arg6);
+        		"input[name=\"phone\"]"));
+        phone.sendKeys("555-877-5105");
     }
 
-    @When("^Admin (\\d+) clicks the \"Edit PHA Record\" button\\.$")
-    public void admin_clicks_the_Edit_PHA_Record_button(int arg1)
+    @When("^Admin clicks the \"Edit PHA Record\" button\\.$")
+    public void admin_clicks_the_Edit_PHA_Record_button()
             throws Throwable
     {
         WebElement submit = browser.findElement(By.cssSelector(
-        		"input [value=\"Edit Personnel Record\"]"));
+        		"input[value=\"Edit Personnel Record\"]"));
         submit.click();
     }
 
-    @Then("^A new Public Health Agent is added with the following information: First Name: Tim Last Name: Agent Address: (\\d+) Oak Hills DriveCity: Capitol City State: North Carolina Zip: (\\d+)-(\\d+) Phone: (\\d+)-(\\d+)-(\\d+) Email: pha@timagent\\.com$")
+    @Then("^A new Public Health Agent is added$")
+    //with the following information: First Name: Tim Last Name: Agent Address: (\\d+) Oak Hills DriveCity: Capitol City State: North Carolina Zip: (\\d+)-(\\d+) Phone: (\\d+)-(\\d+)-(\\d+) Email: pha@timagent\\.com$")
     public void a_new_Public_Health_Agent_is_added_with_the_following_information_First_Name_Tim_Last_Name_Agent_Address_Oak_Hills_DriveCity_Capitol_City_State_North_Carolina_Zip_Phone_Email_pha_timagent_com(
             int arg1, int arg2, int arg3, int arg4, int arg5, int arg6)
             throws Throwable
