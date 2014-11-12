3a24afee514524145deee5be9a46febd6ddbad45
lrbridge <lrbridge@users.noreply.github.com>
2014-11-11 16:21:56 -0800

    refactor to MainMenuSteps to make even more general

--- acceptancetests/edu/ncsu/csc/itrust/acceptance/features/AddPublicHealthAgent.feature
+++ acceptancetests/edu/ncsu/csc/itrust/acceptance/features/AddPublicHealthAgent.feature
@@ -2,7 +2,7 @@ Feature: UC2 Add Public Health Agent
 
   Scenario: 
     Given Admin 9000000001 has authenticated successfully.
-    When Admin clicks the "Add PHA" link under the "Add" menu subheading.
+    When Admin clicks the "Add PHA" link under the "Add" menu subheading
     And Admin enters: first name: Frank last name: Sinatra email: frankie@gmail.com
     And Admin clicks the "Continue to PHA information" link.
     And Admin enters: Address: 123 Oak Hills Drive City: Capitol City State: North Carolina Zip: 28700-0458 Phone: 555-877-5105
--- acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/MainMenuSteps.java
+++ acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/MainMenuSteps.java
@@ -9,15 +9,15 @@ import org.openqa.selenium.WebElement;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.When;
 
-public class PatientHomeSteps {
+public class MainMenuSteps {
 	private final WebDriver browser;
 
-	public PatientHomeSteps(SharedDriver browser) {
+	public MainMenuSteps(SharedDriver browser) {
 		this.browser = browser;
 	}
 	
-    @When("^Patient clicks the \"(.*?)\" link under the \"(.*?)\" menu subheading$")
-    public void admin_clicks_the_Add_PHA_link_under_the_Add_menu_subheading(String link, String subheading)
+    @When("^(.*?) clicks the \"(.*?)\" link under the \"(.*?)\" menu subheading$")
+    public void clicksMenuItem(String person, String link, String subheading)
             throws Throwable
     {
     	List<WebElement> menus = browser.findElements(By.cssSelector(
