0cd2c3657ded4fafaf616898f6389846ec2a05c7
muymoo <matt.momont@gmail.com>
2014-11-15 19:45:18 -0800

    Added "Edit an appointment" to UC22

--- acceptancetests/edu/ncsu/csc/itrust/acceptance/features/ScheduleAnAppointment.feature
+++ acceptancetests/edu/ncsu/csc/itrust/acceptance/features/ScheduleAnAppointment.feature
@@ -1,10 +1,17 @@
 Feature: UC22 - Schedule an Appointment
 
-  Scenario: 
+  Scenario: Add an appointment
     Given Admin "9000000001" has authenticated successfully
     When Admin clicks the "Edit Appointment Types" link under the "Edit" menu subheading
-    And Admin chooses to add a new appointment type named "Immunization" with a duration of 30 minutes
-    And Admin clicks submit to save the new appointment type
-    Then The new "Immunization" appointment type is now included in the list of appointment types, with duration of 30 minutes
+    And Admin chooses to add the appointment type named "Immunization" with a duration of 30 minutes
+    And Admin clicks submit to save the appointment type
+    Then the "Immunization" appointment type is now included in the list of appointment types, with duration of 30 minutes
+
+  Scenario: Edit an appointment duration
+    Given Admin "9000000001" has authenticated successfully
+    When Admin clicks the "Edit Appointment Types" link under the "Edit" menu subheading
+    And Admin chooses to edit the appointment type named "Physical" with a duration of 45 minutes
+    And Admin clicks submit to update the appointment type
+    Then the "Physical" appointment type is now included in the list of appointment types, with updated duration of 45 minutes
 
     
--- acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/EditAppointmentTypeSteps.java
+++ acceptancetests/edu/ncsu/csc/itrust/acceptance/steps/EditAppointmentTypeSteps.java
@@ -12,38 +12,88 @@ import cucumber.api.java.en.When;
 public class EditAppointmentTypeSteps
 {
     private final WebDriver browser;
-    private String          randomDuration;
+    private int             randomDuration;
+    private String          randomString;
 
     public EditAppointmentTypeSteps(SharedDriver browser)
     {
         this.browser = browser;
     }
 
-    @When("^Admin chooses to add a new appointment type named \"(.*?)\" with a duration of (\\d+) minutes$")
+    @When("^Admin chooses to add the appointment type named \"(.*?)\" with a duration of (\\d+) minutes$")
     public void admin_chooses_to_add_a_new_appointment_type_named_with_a_duration_of_minutes(String appointmentName,
             int duration)
             throws Throwable
     {
-        browser.findElement(By.id("name")).sendKeys(appointmentName);
-        randomDuration = String.valueOf(new Random().nextInt(100));
-        browser.findElement(By.id("duration")).sendKeys(randomDuration);
+        randomDuration = new Random().nextInt(100);
+        randomString = generateRandomString();
+
+        browser.findElement(By.id("name")).sendKeys(appointmentName + randomString);
+        browser.findElement(By.id("duration")).sendKeys(String.valueOf(randomDuration));
     }
 
-    @When("^Admin clicks submit to save the new appointment type$")
+    @When("^Admin chooses to edit the appointment type named \"(.*?)\" with a duration of (\\d+) minutes$")
+    public void admin_chooses_to_edit_a_new_appointment_type_named_with_a_duration_of_minutes(String appointmentName,
+            int duration)
+            throws Throwable
+    {
+        randomDuration = new Random().nextInt(100);
+        browser.findElement(By.id("name")).sendKeys(appointmentName);
+        browser.findElement(By.id("duration")).sendKeys(String.valueOf(randomDuration));
+    }
+
+    private static String generateRandomString()
+            throws Exception
+    {
+
+        StringBuffer buffer = new StringBuffer();
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        int charactersLength = characters.length();
+
+        for (int i = 0; i < 10; i++)
+        {
+            double index = Math.random() * charactersLength;
+            buffer.append(characters.charAt((int) index));
+        }
+        return buffer.toString();
+    }
+
+    @When("^Admin clicks submit to save the appointment type$")
     public void admin_clicks_submit_to_save_the_new_appointment_type()
             throws Throwable
     {
         browser.findElement(By.id("add")).click();
     }
 
-    @Then("^The new \"(.*?)\" appointment type is now included in the list of appointment types, with duration of (\\d+) minutes$")
+    @When("^Admin clicks submit to update the appointment type$")
+    public void admin_clicks_submit_to_update_the_new_appointment_type()
+            throws Throwable
+    {
+        browser.findElement(By.id("update")).click();
+    }
+
+    @Then("^the \"(.*?)\" appointment type is now included in the list of appointment types, with duration of (\\d+) minutes$")
     public void the_new_appointment_type_is_now_included_in_the_list_of_appointment_types_with_duration_of_minutes(
             String appointmentType, int duration)
             throws Throwable
     {
         Assert.assertTrue(browser.findElement(By.xpath("//*[@id=\"mainForm\"]/table[2]/tbody")).getText()
-                .contains(appointmentType));
+                .contains(appointmentType + randomString));
+
         Assert.assertTrue(browser.findElement(By.xpath("//*[@id=\"mainForm\"]/table[2]/tbody")).getText()
-                .contains(randomDuration));
+                .contains(String.valueOf(randomDuration)));
+    }
+
+    @Then("^the \"(.*?)\" appointment type is now included in the list of appointment types, with updated duration of (\\d+) minutes$")
+    public void the_new_appointment_type_is_now_included_in_the_list_of_appointment_types_with_updated_duration_of_minutes(
+            String appointmentType, int duration)
+            throws Throwable
+    {
+        Assert.assertTrue(browser.findElement(By.xpath("//*[@id=\"mainForm\"]/table[2]/tbody")).getText()
+                .contains(appointmentType));
+
+        Assert.assertTrue(browser.findElement(By.xpath("//*[@id=\"mainForm\"]/table[2]/tbody")).getText()
+                .contains(String.valueOf(randomDuration)));
     }
 }
